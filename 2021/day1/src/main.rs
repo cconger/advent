63b6aa1a12b890340315de441c722d61a1c6bfe0
Channing Conger <cconger@Armie.local>
2021-12-01 09:18:19 -0800

    Both parts

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -9,11 +9,19 @@ fn main() {
     let values: Vec<i64> = buf_reader.lines().map(|x| { x.unwrap().parse().unwrap() }).collect();
 
     let mut count: i64 = 0;
+    let mut prev = values[0];
+    for v in &values {
+        if *v > prev { count += 1; }
+        prev = *v;
+    }
+    println!("Part 1: {}", count);
+
+    let mut count: i64 = 0;
     for i in 3..values.len() {
         if values[i] > values[i-3] {
             count += 1;
         }
     }
 
-    println!("Increasing: {}", count)
+    println!("Part 2: {}", count);
 }
