911cf17e35f49ddd587a742ce39a49405d43f6c8
Channing Conger <cconger@gmail.com>
2021-12-06 21:39:03 -0800

    Day 7: Whats with the ez stuff?

---
+++ 2021/day7/src/main.rs
@@ -0,0 +1,56 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    // Read file line by line
+    let file = File::open("input.txt").unwrap();
+    let buf_reader = BufReader::new(file);
+
+    let seed: Vec<usize> = buf_reader.lines().next().unwrap().unwrap().split(',').map(|n| { n.parse().unwrap() }).collect();
+    let min = seed.iter().min().unwrap();
+    let max = seed.iter().max().unwrap();
+
+    let mut p = *min;
+    let mut p_2 = *min;
+    let mut c = costlinear(*min, &seed);
+    let mut c_2 = cost(*min, &seed);
+    for i in *min+1..=*max {
+        let nc = cost(i, &seed);
+        if nc < c_2 {
+            c_2 = nc;
+            p_2 = i;
+        }
+        let nc = costlinear(i, &seed);
+        if nc < c {
+            c = nc;
+            p = i;
+        }
+    }
+
+    // Was thinking about doing a binary search, but this was like... small
+
+    println!("Part 1 best pivot {} with Fuel Cost: {}", p, c);
+    println!("Part 2 best pivot {} with Fuel Cost: {}", p_2, c_2);
+}
+
+fn costlinear(pivot:usize, pos: &Vec<usize>) -> usize {
+    pos.iter().fold(0, |acc, v| {
+        if pivot > *v {
+            acc + (pivot - v)
+        } else {
+            acc + (v - pivot)
+        }
+    })
+}
+
+fn cost(pivot: usize, pos: &Vec<usize>) -> usize {
+    pos.iter().fold(0, |acc, v| {
+        let offset: usize;
+        if pivot > *v {
+            offset = pivot - v;
+        } else {
+            offset = v - pivot;
+        }
+        acc + ((offset.pow(2) + offset) / 2)
+    })
+}
